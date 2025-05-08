b5d6154497abc4835883fccff816a014bc796e8e
ErrorTeaPot <github.z5tea@passinbox.com>
2025-05-08 17:37:33 +0200

    Move server user definition into servers default config

--- hosts/servers/default.nix
+++ hosts/servers/default.nix
@@ -8,5 +8,20 @@
     osProber = false;
     latestGenerationMax = 5;
   };
+
   ssh_hardening = true;
+
+  users.users = {
+    errorteapot = {
+      isNormalUser = true;
+      description = "ErrorTeaPot";
+      extraGroups = ["wheel"];
+      shell = pkgs.fish;
+      packages = with pkgs; [
+      ];
+      openssh.authorizedKeys.keys = [
+	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1KhwTRJecS2kYGqAeHXvivJ2iGaKVM/okZql+oJ+Vr errorteapot@TeaPotDesktopL"
+      ];
+    };
+  };
 }
--- hosts/servers/mikoshi/configuration.nix
+++ hosts/servers/mikoshi/configuration.nix
@@ -17,17 +17,6 @@
   # Configure console keymap
   console.keyMap = "us-acentos";
 
-  users.users = {
-    errorteapot = {
-      isNormalUser = true;
-      description = "ErrorTeaPot";
-      extraGroups = ["wheel"];
-      shell = pkgs.fish;
-      packages = with pkgs; [
-      ];
-    };
-  };
-
   environment.systemPackages = with pkgs; [
   ];
 
@@ -36,23 +25,9 @@
 	fish.enable = true;
   };
 
-  # This setups a SSH server
-  /*
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Opinionated: forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
-    };
-  };
-  */
-
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedUDPPorts = [ 22 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
