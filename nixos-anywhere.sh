f903ae8cdafc7112ce0dc89f7dae1715901abff2
ErrorTeaPot <github.z5tea@passinbox.com>
2025-05-12 15:38:54 +0200

    Update script

--- nixos-anywhere.sh
+++ nixos-anywhere.sh
@@ -1,6 +1,8 @@
 #!/bin/fish
 nix --extra-experimental-features "nix-command flakes" \
 	run github:nix-community/nixos-anywhere -- \
-	--generate-hardware-config nixos-generate-config ./hosts/servers/agwe/hardware-configuration.nix/hardware-configuration.nix \
+	#--generate-hardware-config nixos-generate-config ./hosts/servers/agwe/hardware-configuration.nix/hardware-configuration.nix \
 	--flake .#agwe \
-	--target-host errorteapot@193.122.12.251
+	-i ~/.ssh/agwe \
+	--no-substitute-on-destination \
+	--target-host errorteapot@vm2.teapot.eu.org
