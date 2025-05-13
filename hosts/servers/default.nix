dbda9f9ea45e702eb2148fdf5b217ddbb67fe2b6
ErrorTeaPot <github.z5tea@passinbox.com>
2025-05-13 18:17:54 +0200

    Add ssh-key to servers

--- hosts/servers/default.nix
+++ hosts/servers/default.nix
@@ -25,15 +25,8 @@
       ];
       openssh.authorizedKeys.keys = [
 	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHnXHo03QDAr1wWMGeURKgNOkJsholGnzl+8w5O7oyl"
+	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1KhwTRJecS2kYGqAeHXvivJ2iGaKVM/okZql+oJ+Vr"
       ];
     };
-    /*
-    root = {
-      openssh.authorizedKeys.keys = [
-	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1KhwTRJecS2kYGqAeHXvivJ2iGaKVM/okZql+oJ+Vr errorteapot@TeaPotDesktopL"
-	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPQLLFjq1/8alUiLTT1JPSzODrI+dWl0ljf9GmzPd7I errorteapot@TeaPotLaptop"
-      ];
-    };
-    */
   };
 }
