d0fd54157097ad9e32ad1da064f76b7d1dfd605a
ErrorTeaPot <github.z5tea@passinbox.com>
2025-05-15 19:31:40 +0200

    Add home.nix workstation

--- home-manager/workstations/TeaPotDesktopL/home.nix
+++ home-manager/workstations/TeaPotDesktopL/home.nix
@@ -56,6 +56,9 @@
       enable = true;
       userName = "ErrorTeaPot";
       userEmail = "github.z5tea@passinbox.com";
+      extraConfig = {
+	pull.rebase = false;
+      };
     };
     neovim = {
       enable = true;
