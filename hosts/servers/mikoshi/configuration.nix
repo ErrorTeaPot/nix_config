9ecdd840c7e42413d3775a378c25ed32cbe8492b
ErrorTeaPot <github.z5tea@passinbox.com>
2025-05-15 19:39:45 +0200

    Add static ip address

--- hosts/servers/mikoshi/configuration.nix
+++ hosts/servers/mikoshi/configuration.nix
@@ -16,7 +16,17 @@
   nixpkgs = {
     overlays = [];
   };
-  networking.hostName = "mikoshi";
+  networking = {
+    hostName = "mikoshi";
+    interfaces.ens18.ipv4.addresses = [{
+      address = "192.168.67.5";
+      prefixLength = 24;
+    }];
+    defaultGateway = {
+      address = "192.168.67.1";
+      interface = "ens18";
+    };
+  };
 
   # Configure console keymap
   console.keyMap = "us-acentos";
