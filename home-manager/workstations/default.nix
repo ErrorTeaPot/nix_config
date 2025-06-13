656c82f1fb85d51f64c3ef6bdacb4f7423ded1ea
ErrorTeaPot <github.z5tea@passinbox.com>
2025-06-13 09:43:14 +0200

    Move neovim and starship as workstation defaults

--- home-manager/workstations/default.nix
+++ home-manager/workstations/default.nix
@@ -2,4 +2,17 @@
 	imports = [
 		../default.nix
 	];
+
+	# Programs
+	programs = {
+		neovim = {
+			enable = true;
+			viAlias = true;
+			vimAlias = true;
+		};
+	};
+
+	# Modules
+	starship.enable = true;
+	alacritty.enable = true;
 }
