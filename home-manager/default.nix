be542ceb82e1e9dc70018c5136b55f49a52e5f0d
ErrorTeaPot <github.z5tea@passinbox.com>
2025-06-19 10:21:45 +0200

    Create work laptop config

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,7 +1,25 @@
+{pkgs,...}:
 {
 	imports = [
 		../modules/home-manager/default.nix
 	];
+
+	programs = {
+		neovim = {
+			enable = true;
+			viAlias = true;
+			vimAlias = true;
+			defaultEditor = true;
+		};
+		ranger = {
+			enable = true;
+		};
+	};
+
+	nix = {
+		package = pkgs.nix;
+		settings.experimental-features = [ "nix-command" "flakes" ];
+	};
 
 	# Modules
 	fish.enable = true;
---
+++ home-manager/workstations/TeaPotWorkLaptop/home.nix
@@ -0,0 +1,38 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../default.nix
+  ];
+
+  nixpkgs = {
+    overlays = [];
+    config = {
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "glesaout";
+    homeDirectory = "/home/glesaout";
+    packages = with pkgs; [];
+  };
+
+  programs = {
+    home-manager.enable = true;
+  };
+
+  tooling.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.11";
+}
