c9f63f07638c6ec0222da579365630c7306db9be
ErrorTeaPot <gabin.lesaout@etudiant.univ-rennes.fr>
2025-06-28 10:23:39 +0200

    Add config for laptop

--- flake.nix
+++ flake.nix
@@ -78,6 +78,11 @@
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [./home-manager/workstations/TeaPotWorkLaptop/home.nix];
       };
+      "errorteapot@TeaPotLaptop" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [./home-manager/workstations/TeaPotLaptop/home.nix];
+      };
     };
   };
 }
