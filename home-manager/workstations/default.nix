08cb2939f5631950e792cc05fbe713fb95816239
ErrorTeaPot <github.z5tea@passinbox.com>
2025-07-17 09:17:57 +0200

    format buffer

--- home-manager/workstations/default.nix
+++ home-manager/workstations/default.nix
@@ -1,43 +1,43 @@
 {
-	pkgs,
-	...
+  pkgs,
+  ...
 }:
 {
-	imports = [
-		../default.nix
-	];
+  imports = [
+    ../default.nix
+  ];
 
-	# Programs
-	programs = {
-		git = {
-			enable = true;
-			extraConfig = {
-				pull.rebase = false;
-			};
-		};
-		home-manager.enable = true;
-	};
+  # Programs
+  programs = {
+    git = {
+      enable = true;
+      extraConfig = {
+        pull.rebase = false;
+      };
+    };
+    home-manager.enable = true;
+  };
 
-	home = {
-		packages = with pkgs; [
-			lazygit
-			btop
-			nerd-fonts.caskaydia-cove
-			tree
-			nixfmt-rfc-style
-		];
-		sessionPath = [
-			"$HOME/.local/bin"
-			"$HOME/.config/cargo/bin"
-		];
-		sessionVariables = {
-			RUSTUP_HOME = "$HOME/.config/rustup";
-			XDG_CONFIG_HOME = "$HOME/.config";
-			CARGO_HOME = "$HOME/.config/cargo";
-		};
-	};
+  home = {
+    packages = with pkgs; [
+      lazygit
+      btop
+      nerd-fonts.caskaydia-cove
+      tree
+      nixfmt-rfc-style
+    ];
+    sessionPath = [
+      "$HOME/.local/bin"
+      "$HOME/.config/cargo/bin"
+    ];
+    sessionVariables = {
+      RUSTUP_HOME = "$HOME/.config/rustup";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      CARGO_HOME = "$HOME/.config/cargo";
+    };
+  };
 
-	# Modules
-	starship.enable = true;
-	#alacritty.enable = true;
+  # Modules
+  starship.enable = true;
+  #alacritty.enable = true;
 }
