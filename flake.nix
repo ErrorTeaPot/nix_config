028c949b2b417f4875bcb5347315fc584e1e580d
ErrorTeaPot <github.z5tea@passinbox.com>
2025-08-05 09:27:02 +0200

    flake.nix : add hardware config in mikoshi
    Added hardware-configuration.nix as a module in mikoshi to be able to
    generate it with nixos-anywhere

--- flake.nix
+++ flake.nix
@@ -18,71 +18,75 @@
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    disko,
-    sops-nix,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      TeaPotDesktopL = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      disko,
+      sops-nix,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+    in
+    {
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        TeaPotDesktopL = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
             ./hosts/workstations/TeaPotDesktopL/configuration.nix
-            disko.nixosModules.disko
-	    sops-nix.nixosModules.sops
-          ];
-      };
-      mikoshi = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-            ./hosts/servers/mikoshi/configuration.nix
             disko.nixosModules.disko
             sops-nix.nixosModules.sops
           ];
-      };
-      agwe = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
+        };
+        mikoshi = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/servers/mikoshi/configuration.nix
+            disko.nixosModules.disko
+            sops-nix.nixosModules.sops
+            ./hosts/servers/mikoshi/hardware-configuration.nix
+          ];
+        };
+        agwe = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
             ./hosts/servers/agwe/configuration.nix
             disko.nixosModules.disko
             sops-nix.nixosModules.sops
             #./hosts/servers/agwe/hardware-configuration.nix
           ];
+        };
       };
-    };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      "errorteapot@TeaPotDesktopL" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [./home-manager/workstations/TeaPotDesktopL/home.nix];
-      };
-      "errorteapot@mikoshi" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
+      homeConfigurations = {
+        "errorteapot@TeaPotDesktopL" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [ ./home-manager/workstations/TeaPotDesktopL/home.nix ];
+        };
+        "errorteapot@mikoshi" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
             ./home-manager/servers/mikoshi/home.nix
-        ];
-      };
-      "glesaout@TeaPotWorkLaptop" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [./home-manager/workstations/TeaPotWorkLaptop/home.nix];
-      };
-      "errorteapot@TeaPotLaptop" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [./home-manager/workstations/TeaPotLaptop/home.nix];
+          ];
+        };
+        "glesaout@TeaPotWorkLaptop" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [ ./home-manager/workstations/TeaPotWorkLaptop/home.nix ];
+        };
+        "errorteapot@TeaPotLaptop" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [ ./home-manager/workstations/TeaPotLaptop/home.nix ];
+        };
       };
     };
-  };
 }
