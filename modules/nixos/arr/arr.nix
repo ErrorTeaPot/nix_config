e88e35110abd002679d7bb9d34e37b4996cbd495
ErrorTeaPot <github.z5tea@passinbox.com>
2025-11-30 22:01:51 +0100

    arr : add arr stack module

---
+++ modules/nixos/arr/arr.nix
@@ -0,0 +1,53 @@
+{ config, lib, ... }:
+with lib;
+let
+  cfg = config.arr;
+  url = "jellyfin.teapot.eu.org";
+  port = 8096;
+in
+{
+  options.arr = {
+    enable = mkEnableOption "Enables the *arr stack";
+    vpnConf = mkOption { type = types.path; };
+  };
+
+  config = mkIf cfg.enable {
+    nixarr = {
+      enable = true;
+      mediaDir = "/data/media";
+      stateDir = "/data/media/.state/nixarr";
+
+      vpn = {
+        enable = true;
+        wgConf = config.arr.vpnConf; # Fix: utilise config au lieu de cfg
+      };
+
+      jellyfin.enable = true;
+
+      # Retire services.caddy d'ici ! Il va en bas
+
+      transmission = {
+        enable = true;
+        vpn.enable = true;
+        peerPort = 51820;
+      };
+
+      recyclarr.enable = true;
+      bazarr.enable = true;
+      lidarr.enable = true;
+      prowlarr.enable = true;
+      radarr.enable = true;
+      readarr.enable = true;
+      sonarr.enable = true;
+      jellyseerr.enable = true;
+    };
+
+    # Déplace services.caddy ICI, à la racine
+    services.caddy.virtualHosts."jellyfin.teapot.eu.org" = {
+      extraConfig = ''
+        reverse_proxy http://localhost:${toString port}
+      '';
+      useACMEHost = "teapot.eu.org";
+    };
+  };
+}
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,11 +1,12 @@
 {
-	imports = [
-		./systemd_hardening/systemd_hardening.nix
-		./locale/locale.nix
-		./boot/boot.nix
-		./ssh_hardening/ssh_hardening.nix
-		./reverse_proxy/reverse_proxy.nix
-		./rss/rss.nix
-		./nextcloud/nextcloud.nix
-	];
+  imports = [
+    ./systemd_hardening/systemd_hardening.nix
+    ./locale/locale.nix
+    ./boot/boot.nix
+    ./ssh_hardening/ssh_hardening.nix
+    ./reverse_proxy/reverse_proxy.nix
+    ./rss/rss.nix
+    ./nextcloud/nextcloud.nix
+    ./arr/arr.nix
+  ];
 }
