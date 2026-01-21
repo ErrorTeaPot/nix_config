0e0cd0334eb5454c596b0d5eb314fa9860e3e85a
ErrorTeaPot <github.z5tea@passinbox.com>
2026-01-21 14:17:55 +0100

    arr : disable for now

--- modules/nixos/arr/arr.nix
+++ modules/nixos/arr/arr.nix
@@ -15,85 +15,87 @@ in
   };
 
   config = mkIf cfg.enable {
-    nixarr = {
-      enable = true;
-      mediaDir = "/data/media";
-      stateDir = "/data/media/.state/nixarr";
-
-      vpn = {
+    /*
+      nixarr = {
         enable = true;
-        wgConf = config.arr.vpnConf;
-      };
+        mediaDir = "/data/media";
+        stateDir = "/data/media/.state/nixarr";
 
-      jellyfin.enable = true;
+        vpn = {
+          enable = true;
+          wgConf = config.arr.vpnConf;
+        };
 
-      transmission = {
-        enable = true;
-        vpn.enable = true;
-        peerPort = vpn_forwarded_port;
-      };
+        jellyfin.enable = true;
 
-      recyclarr = {
-        enable = true;
-        configuration = {
-          sonarr = {
-            series = {
-              base_url = "http://localhost:${toString sonarr_port}";
-              api_key = "!env_var SONARR_API_KEY";
-              quality_definition = {
-                type = "series";
+        transmission = {
+          enable = true;
+          vpn.enable = true;
+          peerPort = vpn_forwarded_port;
+        };
+
+        recyclarr = {
+          enable = true;
+          configuration = {
+            sonarr = {
+              series = {
+                base_url = "http://localhost:${toString sonarr_port}";
+                api_key = "!env_var SONARR_API_KEY";
+                quality_definition = {
+                  type = "series";
+                };
+                delete_old_custom_formats = true;
               };
-              delete_old_custom_formats = true;
             };
-          };
-          radarr = {
-            movies = {
-              base_url = "http://localhost:${toString radarr_port}";
-              api_key = "!env_var RADARR_API_KEY";
-              quality_definition = {
-                type = "movie";
-              };
-              delete_old_custom_formats = true;
-              include = [
-                # Movie
-                { template = "radarr-quality-definition-movie"; }
-                { template = "radarr-quality-profile-remux-web-1080p"; }
-                { template = "radarr-custom-formats-remux-web-1080p"; }
+            radarr = {
+              movies = {
+                base_url = "http://localhost:${toString radarr_port}";
+                api_key = "!env_var RADARR_API_KEY";
+                quality_definition = {
+                  type = "movie";
+                };
+                delete_old_custom_formats = true;
+                include = [
+                  # Movie
+                  { template = "radarr-quality-definition-movie"; }
+                  { template = "radarr-quality-profile-remux-web-1080p"; }
+                  { template = "radarr-custom-formats-remux-web-1080p"; }
 
-                # Anime
-                { template = "radarr-quality-definition-anime"; }
-                { template = "radarr-quality-profile-anime"; }
-                { template = "radarr-custom-formats-anime"; }
-              ];
-              custom_formats = [
-                {
-                  trash_ids = [
-                    "064af5f084a0a24458cc8ecd3220f93f" # Uncensored
-                    "9172b2f683f6223e3a1846427b417a3d" # VOSTFR
-                  ];
-                  assign_scores_to = [
-                    {
-                      name = "Remux-1080p - Anime";
-                    }
-                  ];
-                }
-              ];
+                  # Anime
+                  { template = "radarr-quality-definition-anime"; }
+                  { template = "radarr-quality-profile-anime"; }
+                  { template = "radarr-custom-formats-anime"; }
+                ];
+                custom_formats = [
+                  {
+                    trash_ids = [
+                      "064af5f084a0a24458cc8ecd3220f93f" # Uncensored
+                      "9172b2f683f6223e3a1846427b417a3d" # VOSTFR
+                    ];
+                    assign_scores_to = [
+                      {
+                        name = "Remux-1080p - Anime";
+                      }
+                    ];
+                  }
+                ];
+              };
             };
           };
         };
+        bazarr.enable = true;
+        prowlarr.enable = true;
+        radarr.enable = true;
+        sonarr.enable = true;
+        jellyseerr.enable = true;
       };
-      bazarr.enable = true;
-      prowlarr.enable = true;
-      radarr.enable = true;
-      sonarr.enable = true;
-      jellyseerr.enable = true;
-    };
 
-    services.caddy.virtualHosts.${toString url} = {
-      extraConfig = ''
-        reverse_proxy http://localhost:${toString jellyfin_port}
-      '';
-      useACMEHost = "teapot.eu.org";
-    };
+      services.caddy.virtualHosts.${toString url} = {
+        extraConfig = ''
+          reverse_proxy http://localhost:${toString jellyfin_port}
+        '';
+        useACMEHost = "teapot.eu.org";
+      };
+    */
   };
 }
