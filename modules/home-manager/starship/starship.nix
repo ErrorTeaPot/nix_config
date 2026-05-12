cc5d8d08c786978d5f9c1feb9e88b9d8cb93c7e7
ErrorTeaPot <glesaout@almond.eu>
2026-05-12 09:39:35 +0200

    modules/home-manager/starship : change palette

--- modules/home-manager/starship/starship.nix
+++ modules/home-manager/starship/starship.nix
@@ -1,4 +1,8 @@
-{ pkgs, lib, config, ... }:
+{
+  lib,
+  config,
+  ...
+}:
 
 {
   options = {
@@ -11,27 +15,36 @@
     programs.starship = {
       enable = true;
       settings = {
-        palette = "material";
+        palette = "rose-pine";
         character = {
-          error_symbol = "[🌶️](bold #ff5555)";
-          success_symbol = "[🌶️](bold #50fa7b)";
+          error_symbol = "[🌶️ ](bold #ff5555)";
+          success_symbol = "[🌶️ ](bold #50fa7b)";
         };
         username = {
           format = "[$user]($style) on ";
         };
-        palettes.material = {
-          black   = "#666666";
-          red     = "#eb606b";
-          green   = "#c3e88d";
-          yellow  = "#f7eb95";
-          blue    = "#80cbc4";
-          magenta = "#ff2f90";
-          cyan    = "#aeddff";
-          white   = "#ffffff";
+        palettes = {
+          material = {
+            black = "#666666";
+            red = "#eb606b";
+            green = "#c3e88d";
+            yellow = "#f7eb95";
+            blue = "#80cbc4";
+            magenta = "#ff2f90";
+            cyan = "#aeddff";
+            white = "#ffffff";
+          };
+          rose-pine = {
+            overlay = "#26233a";
+            love = "#eb6f92";
+            gold = "#f6c177";
+            rose = "#ebbcba";
+            pine = "#31748f";
+            foam = "#9ccfd8";
+            iris = "#c4a7e7";
+          };
         };
-        #};
       };
     };
   };
 }
-
