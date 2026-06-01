fdd1a678ade9bbbb3814f02b9b9ea9c43917bfb4
ErrorTeaPot <glesaout@almond.eu>
2026-06-01 13:31:41 +0200

    nixOS/wm-setup : fmt code

--- modules/nixos/wm-setup/wm-setup.nix
+++ modules/nixos/wm-setup/wm-setup.nix
@@ -1,40 +1,47 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
 let
   # Shorter name to access a final setting
   cfg = config.wm-setup;
-in {
+in
+{
   options.wm-setup = {
     enable = mkEnableOption "Window manager setup";
   };
 
   config = mkIf cfg.enable {
-  # Enable keyring
-  services.gnome = {
-    gnome-keyring.enable = true;
-    gcr-ssh-agent.enable = true;
-  };
-  security.pam.services = {
-    sddm.enableGnomeKeyring = true;
-  };
 
-  # Idle daemon
-  services.hypridle.enable = true;
-
-  # Enable programs
-  programs = {
-    hyprland = {
-      enable = true;
-      withUWSM = true;
+    # Enable keyring
+    services.gnome = {
+      gnome-keyring.enable = true;
+      gcr-ssh-agent.enable = true;
     };
-    ssh = {
-      startAgent = false;
-      enableAskPassword = true;
+    security.pam.services = {
+      sddm.enableGnomeKeyring = true;
     };
-  };
 
-  environment.systemPackages = with pkgs; [
-    libsecret
-  ];
-};
+    # Idle daemon
+    services.hypridle.enable = true;
+
+    # Enable programs
+    programs = {
+      hyprland = {
+        enable = true;
+        withUWSM = true;
+      };
+      ssh = {
+        startAgent = false;
+        enableAskPassword = true;
+      };
+    };
+
+    environment.systemPackages = with pkgs; [
+      libsecret
+    ];
+  };
 }
