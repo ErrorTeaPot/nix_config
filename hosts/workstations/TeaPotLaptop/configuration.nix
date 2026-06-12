836c6ae4274ccccf92759f609e9ef0ee806d4165
ErrorTeaPot <github.z5tea@passinbox.com>
2026-06-12 23:03:07 +0200

    nixos/laptop : enable bluetooth, steam and veracrypt

--- hosts/workstations/TeaPotLaptop/configuration.nix
+++ hosts/workstations/TeaPotLaptop/configuration.nix
@@ -52,6 +52,7 @@
       packages = with pkgs; [
         file-roller
         yazi
+        veracrypt
       ];
       openssh.authorizedKeys.keys = [ ];
     };
@@ -98,10 +99,34 @@
     virt-manager = {
       enable = true;
     };
+    steam.enable = true;
   };
 
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.tumbler.enable = true; # Thumbnail support for images
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
+  services.blueman.enable = true;
 
   # Fingerprint auth
   services.fprintd.enable = true;
