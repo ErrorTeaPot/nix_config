9fbc1a5598f3798f17cde3fa65d795d7bb184ca7
ErrorTeaPot <github.z5tea@passinbox.com>
2026-06-13 19:51:24 +0200

    nixos/hardening : graphene allocator and nix daemon restriction

--- modules/nixos/hardening/hardening.nix
+++ modules/nixos/hardening/hardening.nix
@@ -10,10 +10,16 @@ let
   cfg = config.hardening;
 in
 {
+  # Based on https://wiki.nixos.org/wiki/NixOS_Hardening
   options.hardening = {
     enable = mkEnableOption "Harden things";
   };
 
   config = mkIf cfg.enable {
+    # Allow only users to connect to the Nix daemon
+    nix.settings.allowed-users = [ "@users" ];
+
+    # Use graphene allocator
+    environment.memoryAllocator.provider = "graphene-hardened";
   };
 }
